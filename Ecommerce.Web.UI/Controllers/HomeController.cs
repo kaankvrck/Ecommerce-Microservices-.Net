136611b629693423d11168593ea1bd79e0069c89
BAHA <BAHA@DESKTOP-6TB0GCH>
2024-01-07 20:23:10 +0300

    Web.UI

---
+++ Ecommerce.Web.UI/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Ecommerce.Web.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ecommerce.Web.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Ecommerce.Web.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecommerce.Web.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
