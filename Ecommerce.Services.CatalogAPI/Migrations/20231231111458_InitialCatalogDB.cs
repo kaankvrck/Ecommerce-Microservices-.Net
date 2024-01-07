ca04eec860e08b1ed18e2510c431695b83f1373c
BAHA <BAHA@DESKTOP-6TB0GCH>
2024-01-07 15:08:36 +0300

    Catalog

---
+++ Ecommerce.Services.CatalogAPI/Migrations/20231231111458_InitialCatalogDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Services.CatalogAPI.Migrations
+{
+    public partial class InitialCatalogDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Catalogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Category = table.Column<string>(type: "text", nullable: false),
+                    Brand = table.Column<string>(type: "text", nullable: false),
+                    Descripton = table.Column<string>(type: "text", nullable: true),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<double>(type: "double", nullable: false),
+                    Photo = table.Column<byte[]>(type: "byte[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Catalogs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Catalogs");
+        }
+    }
+}
