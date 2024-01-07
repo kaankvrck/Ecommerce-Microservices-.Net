010f2a8208532055ec837248b4e563eb8239171c
BAHA <BAHA@DESKTOP-6TB0GCH>
2024-01-07 15:36:43 +0300

    Catalog

--- Ecommerce.Services.CatalogAPI/Models/Catalog.cs
+++ Ecommerce.Services.CatalogAPI/Models/Catalog.cs
@@ -4,5 +4,11 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        public string? Descripton { get; set; }
+        public int Stock { get; set; }
+        public double Price { get; set; }
+        public byte[] Photo { get; set; }
     }
 }
