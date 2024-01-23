f64861876e12078c31095d14c09448b2634bc246
cbyilmaz <151728558+cbyilmaz@users.noreply.github.com>
2024-01-23 03:38:12 +0300

    Items with 0 stock is no shown on Shop page Item quantity cannot be increased beyond its stock

--- Ecommerce.Web.UI/wwwroot/js/templatemo.js
+++ Ecommerce.Web.UI/wwwroot/js/templatemo.js
@@ -1,68 +1,75 @@
 'use strict';
 $(document).ready(function () {
 
-  // Accordion
-  var all_panels = $('.templatemo-accordion > li > ul').hide();
+    // Accordion
+    var all_panels = $('.templatemo-accordion > li > ul').hide();
 
-  $('.templatemo-accordion > li > a').click(function () {
-    console.log('Hello world!');
-    var target = $(this).next();
-    if (!target.hasClass('active')) {
-      all_panels.removeClass('active').slideUp();
-      target.addClass('active').slideDown();
-    }
-    return false;
-  });
-  // End accordion
+    $('.templatemo-accordion > li > a').click(function () {
+        console.log('Hello world!');
+        var target = $(this).next();
+        if (!target.hasClass('active')) {
+            all_panels.removeClass('active').slideUp();
+            target.addClass('active').slideDown();
+        }
+        return false;
+    });
+    // End accordion
 
-  // Product detail
-  $('.product-links-wap a').click(function () {
-    var this_src = $(this).children('img').attr('src');
-    $('#product-detail').attr('src', this_src);
-    return false;
-  });
+    // Product detail
+    $('.product-links-wap a').click(function () {
+        var this_src = $(this).children('img').attr('src');
+        $('#product-detail').attr('src', this_src);
+        return false;
+    });
 
-  // Increment quantity for a specific product
-  $('.btn-plus').click(function () {
-    var productId = $(this).data('product-id');
-    var varValue = $("#var-value-" + productId);
-    $('[id^="var-value-"]').not(varValue).text(0);
-    var val = parseInt(varValue.text());
-    val++;
+    // Increment quantity for a specific product
+    $('.btn-plus').click(function () {
+        var productId = $(this).data('product-id');
+        var varValue = $("#var-value-" + productId);
+        $('[id^="var-value-"]').not(varValue).text(0);
 
-   $('input[name="adet"]').val(val);
+        var val = parseInt(varValue.text());
+        var stock = parseInt($("#productstock-" + productId).val());
 
-      $('input[name="choosenid"]').val(productId);
+        if (val < stock) {
+            val++;
+        } else {
+            alert("Quantity cannot exceed stock limit");
+            return false;
+        }
 
-    varValue.text(val);
-    $("#product-quantity-" + productId).val(val);
+        $('input[name="adet"]').val(val);
+        $('input[name="choosenid"]').val(productId);
 
-    return false;
-  });
+        varValue.text(val);
+        $("#product-quantity-" + productId).val(val);
 
-  // Decrement quantity for a specific product
-  $('.btn-minus').click(function () {
-    var productId = $(this).data('product-id');
-    var varValue = $("#var-value-" + productId);
+        return false;
+    });
 
-    var val = parseInt(varValue.text());
-    val = (val > 0) ? val - 1 : 0;
+    // Decrement quantity for a specific product
+    $('.btn-minus').click(function () {
+        var productId = $(this).data('product-id');
+        var varValue = $("#var-value-" + productId);
 
-    varValue.text(val);
-    $("#product-quantity-" + productId).val(val);
+        var val = parseInt(varValue.text());
+        val = (val > 0) ? val - 1 : 0;
 
-    return false;
-  });
+        varValue.text(val);
+        $("#product-quantity-" + productId).val(val);
 
-  $('.btn-size').click(function () {
-    var this_val = $(this).html();
-    $("#product-size").val(this_val);
-    $(".btn-size").removeClass('btn-secondary');
-    $(".btn-size").addClass('btn-success');
-    $(this).removeClass('btn-success');
-    $(this).addClass('btn-secondary');
-    return false;
-  });
-  // End product detail
+        return false;
+    });
+
+    $('.btn-size').click(function () {
+        var this_val = $(this).html();
+        $("#product-size").val(this_val);
+        $(".btn-size").removeClass('btn-secondary');
+        $(".btn-size").addClass('btn-success');
+        $(this).removeClass('btn-success');
+        $(this).addClass('btn-secondary');
+        return false;
+    });
+    // End product detail
 
 });
